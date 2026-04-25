cb790162fcaadb5cbafabbe6ce7c905d892f1c7d
vittallyyaaa <vitaliymotsniyr@gmail.com>
2026-04-25 16:49:23 +0300

    Started creating Oculus Website

--- react-demo/src/App.tsx
+++ react-demo/src/App.tsx
@@ -1,11 +1,7 @@
-import ListGroup from "./components/ListGroup";
-import Navbar from "./components/Navbar";
-
 function App() {
   return (
     <div>
-      <Navbar />
-      <ListGroup />
+      1
     </div>
   );
 }
--- react-demo/src/Message.tsx
+++
@@ -1,6 +0,0 @@
-function Message() {
-    let name = 'Vitaliy'
-    return <h1>Hello, {name}</h1>;
-}
-
-export default Message;
